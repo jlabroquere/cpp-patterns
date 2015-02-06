8193d2296f29e1835219c3086db9f81275ceee28
Jeremie Labroquere <jeremie.labroquere@inria.fr>
2015-02-06 16:52:19 +0100

    Structure for the project.

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # cpp-patterns
-Some common design patterns in c++
+Some common design patterns implemented in C++. All the design patterns are implemented as unit tests, providing examples on how to use them.
+All the design patterns are based on the sourcemaking.com website.
+
+## Requirements:
+- Qt version > 5
+
+## Build steps:
+- mkdir build
+- cd build
+- ccmake ..
+- make
+- make test
---
+++ tst/creationalPatterns/abstractFactoryTest.cpp
@@ -0,0 +1,42 @@
+// Version: $Id$
+//
+//
+
+// Commentary:
+//
+//
+
+// Change Log:
+//
+//
+
+// Code:
+
+#include "abstractFactoryTest.h"
+
+void abstractFactoryTestCase::initTestCase(void)
+{
+};
+void abstractFactoryTestCase::init(void)
+{
+};
+
+void abstractFactoryTestCase::testVerify(void)
+{
+    QVERIFY(1 == 1);
+};
+
+void abstractFactoryTestCase::cleanupTestCase(void)
+{
+
+}
+
+void abstractFactoryTestCase::cleanup(void)
+{
+
+}
+
+QTEST_MAIN(abstractFactoryTestCase)
+
+//
+// file ends here
